d0be0238aa2518a9724978339610f3bd4e09038c
Abdelrahman <abdo@example.com>
2023-08-10 15:25:21 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is file named hello world
+ *
+ * description: nothing to do here
+ *
+ * return: 0 Always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
