e7f77c4839cf8ff7f8f13f05bb7204ccfb695f83
Abdelrahman <abdo@example.com>
2023-08-10 17:35:16 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-	return (0)
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
 
 }
