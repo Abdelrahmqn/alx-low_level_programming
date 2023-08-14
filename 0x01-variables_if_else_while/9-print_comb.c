964cf18148a185606749945445ca1e1249dbae1d
Abdelrahman <abdo@example.com>
2023-08-14 13:42:00 -0700

    welcome to C

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: write the numbers 0 to 10 with ,
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+		for (i = 48 ; i < 57; i++)
+{
+			putchar(i);
+			putchar(' ');
+			putchar(',');
+}
+}
+	putchar('\n');
+	return (0);
+}
+
+
+}
