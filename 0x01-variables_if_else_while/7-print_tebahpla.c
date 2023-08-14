5ea9d5f1187cf1239de464b77139fb4332202b28
Abdelrahman <abdo@example.com>
2023-08-14 12:42:03 -0700

    welcome to C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * discrpition: print all alphapet latters
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a)
+{
+		putchar(ch);
+		ch++;
+}
+	putchar('\n');
+
+	return (0);
+}
