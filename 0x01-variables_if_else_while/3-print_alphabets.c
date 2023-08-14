94d232f010333b369dac75fd469a079c572b9fdd
Abdelrahman <abdo@example.com>
2023-08-14 10:33:55 -0700

    welcome to C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: write the alphabet in lower case then upper case
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+	putchar(ch);
+		ch++;
+}
+	putchar('\n');
+{
+	char ch = 'A';
+
+	while (ch <= 'Z')
+{
+		putchar(ch);
+		ch++;
+}
+	putchar('\n');
+}
+
+	return (0);
+}
