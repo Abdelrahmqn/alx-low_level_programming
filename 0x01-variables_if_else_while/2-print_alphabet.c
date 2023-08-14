2b08f2b4ed79be75c6e8ff716c8f275f107f0472
Abdelrahman <abdo@example.com>
2023-08-14 08:28:11 -0700

    welcome to C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,18 @@
-f#include<stdio.h>
+#include <stdio.h>
 /**
 * main - Entry point
+*
+* Description: form the character a to z
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	char ch = a;
-	puchar("%c\n", a);
-	c++
+	char ch = 'a';
+	while (ch <= 'z');
+	{
+	putchar(ch);
+	ch++;
+	}
 	return (0);
 }
