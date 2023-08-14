605e265a773d0ca6f068d00e1c63e0c1b9af92cd
Abdelrahman <abdo@example.com>
2023-08-14 11:52:08 -0700

    welcome to C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: write the letters 1 to 9 <base 10>
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int i = '48';
+
+	while (i <= '57')
+
+	{
+		putchar(i);
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+}
