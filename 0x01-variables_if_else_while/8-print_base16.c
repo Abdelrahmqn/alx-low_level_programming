1aa8e981d81762fc29c116e2f1bc1da762e1aa38
Abdelrahman <abdo@example.com>
2023-08-14 12:57:05 -0700

    welcome to C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: write the alphabet in lower case then upper case
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+{
+		putchar(ch);
+		ch++;
+}
+{
+	char ch = 'a';
+
+	while (ch <= 'f')
+{
+		putchar(ch);
+		ch++;
+}
+	putchar('\n');
+
+}
+	return (0);
+}
