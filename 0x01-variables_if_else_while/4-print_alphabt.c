86a5d790eb906b06a92b9a2b51ee41ba9487bcc4
Abdelrahman <abdo@example.com>
2023-08-14 10:47:02 -0700

    welcome to C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * discrpition: print all alphapet latters
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	while ('e', 'q' > 'z')
+	{
+		putchar(ch)
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
