e94259c0bb1d2f25720a83fefa21183181f8f453
Abdelrahman <abdo@example.com>
2023-08-16 03:18:05 -0700

    welcome to the C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Description: print this world
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	{char ch = 'putchar'
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
