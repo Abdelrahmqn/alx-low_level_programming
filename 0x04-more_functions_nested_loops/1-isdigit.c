2a12640c4e409b30fa9a885c2ec37d13b12cb203
Abdelrahman <abdo@example.com>
2023-08-17 12:29:32 -0700

    welcome to C

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main -Entry point
+ *
+ * @c: input for alphabet
+ *
+ * Description: check for a digit (0 through 9).
+ *
+ * Return: return 1 if c digit or return 0 if not
+*/
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
