94a42ae38d985732db11ba34f83888a1efe75e39
Abdelrahman <abdo@example.com>
2023-08-17 15:45:38 -0700

    well cod

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_putchar - entry point
+ *
+ *print from 0 to 9, followed by a new line
+ *
+ *Return: Always 0.
+*/
+void print_numbers(void);
+{ for (c = 48; c < 58; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
+}
