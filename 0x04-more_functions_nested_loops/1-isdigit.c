ff9ada92f803e29d727b082e9e347b91d028f2a9
Abdelrahman <abdo@example.com>
2023-08-17 12:38:09 -0700

    welcome to C

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,8 @@
-#include <main.h>
+#include "main.h"
 /**
  * main -Entry point
  *
  * @c: input for alphabet
- *
- * Description: check for a digit (0 through 9).
  *
  * Return: return 1 if c digit or return 0 if not
 */
