41b228981a5cd08ce3e33025c552f92e2c7493c1
Abdelrahman <abdo@example.com>
2023-08-29 13:28:13 -0700

    c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_chessboard - Write a function that prints the chessboard.
+ *
+ * @a: print the board
+*/
+void print_chessboard(char (*a)[8])
+{
+	printf("chessboard");
+		return (0);
+}
