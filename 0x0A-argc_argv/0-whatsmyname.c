5a6c0d5c385357bac5f6b3d8a4bdf09c85e8711a
Abdelrahman <abdo@example.com>
2023-09-03 08:04:13 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * followed by n l If you rename the program, it will print the new name
+ * @argv: master argv
+ * @argc: master argc
+ * Return: Always 0 (Success)
+*/
+int main(int argc[], char *argv[])
+{
+	printf("%s\n", argv[argc]);
+	return (0);
+}
