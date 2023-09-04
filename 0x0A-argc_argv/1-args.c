6d41bc1e035376b462eb64585ff3e7fd1c07e098
Abdelrahman <abdo@example.com>
2023-09-04 07:23:16 -0700

    c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,14 @@
  *@argv: array of arguments
  *Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
 }
