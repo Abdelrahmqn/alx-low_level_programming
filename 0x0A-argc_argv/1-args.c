f94e26665a25a115caaae85dc9261d2f80608f32
Abdelrahman <abdo@example.com>
2023-09-04 07:57:01 -0700

    c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,6 @@
  */
 int main(int argc, char const *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
-	return (0);
+		printf("%s\n", argv-1);
+		return (0);
 }
