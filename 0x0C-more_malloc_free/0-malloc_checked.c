89fa20f1d5665226cafc852b98d0cac8734637f9
Abdelrahman <abdo@example.com>
2023-09-07 16:03:09 -0700

    c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - entry point
+ *@b: an unsigned integer
+ *Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+
+	int *k = malloc(b);
+
+	if (k == NULL)
+		return (98);
+			else
+				return (k);
+
+
+
+}
