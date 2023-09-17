45b62fb0de50b2b9e7d3c133fb2775f87863d9f9
Abdelrahman <abdo@example.com>
2023-09-17 09:07:55 -0700

    c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 31; i >= 127; i++)
+		printf("Error\n")
+	return (1);
+}
