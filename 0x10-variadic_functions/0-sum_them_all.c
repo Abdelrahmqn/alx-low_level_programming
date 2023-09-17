de6debe67c3695e82377427ecb0c56f1db701f2d
Abdelrahman <abdo@example.com>
2023-09-17 15:27:17 -0700

    c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@ int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0, i = n;
 va_list ap;
-if (n != 0)
-	return (0);
+if (!n)	
+return (0);
 	va_start(ap, n);
 while (i--)
 	sum += va_arg(ap, int);
