9023f410239a37bce358cc9f4d3be7ada444c0df
Abdelrahman <abdo@example.com>
2023-09-18 14:54:31 -0700

    c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#includ "dog.h"
+/**
+ *init_dog - dog's information
+ *@d: int
+ *@name: char
+ *@age: number
+ *@owner: name
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog my_dog;
+}
