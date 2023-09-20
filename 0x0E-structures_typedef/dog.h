a3683efd25773a1c8421478e4d53442fc1ccbad0
Abdelrahman <abdo@example.com>
2023-09-20 02:17:14 -0700

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,11 @@ struct dog
 	char *name;
 		float age;
 			char *owner;
-}typedef struct dog_t;
+};
 /**
  * dog_t - typedef for struct dog
  */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
