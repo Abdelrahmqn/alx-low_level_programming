cfe06ecdfbbdfaa773f18056f01699a208e653e5
Abdelrahman <abdo@example.com>
2023-09-20 00:27:29 -0700

    c

---
+++ 0x06-pointers_arrays_strings/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+*init_dog - writte a function that initalize a varible of type.
+*@d:int
+*@name: char
+*@age: float
+*@owner: char
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name;
+d->float;
+d->owner
+}
