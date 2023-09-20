044e4b0ae9e85f2db5f4762946bc62661a726d1b
Abdelrahman <abdo@example.com>
2023-09-20 01:21:40 -0700

    c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *print_dog - dog's information
+ *@d: char
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+	if (d == NULL)
+		printf("(nill)\n");
+	else
+		(name == NULL)
+			printf("Name: (nil)\n");
+
+	}
+}
