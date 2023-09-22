5f67223a4d5fe217b0960668581b751975bf5163
Abdelrahman <abdo@example.com>
2023-09-22 09:45:02 -0700

    linked lists in c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - prints the number of elements in a list-t.
+ *
+ * @h: head
+ *
+ * Return: elemnts.
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *uniq = h;
+
+	while (temp != NULL)
+	{
+		count++;
+	}
+	return (count);
+}
