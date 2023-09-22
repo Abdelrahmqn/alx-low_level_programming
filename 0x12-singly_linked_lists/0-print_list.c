437aa4feb22acb775661b5b70d8e3ae94ff03eef
Abdelrahman <abdo@example.com>
2023-09-21 18:30:47 -0700

    c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,29 @@
 #include "lists.h"
+
 /**
  * print_list - prints all elemnts of a list_t list.
  *
- * @h: ptr
+ * @h: inputs pointer
  *
- * return : If str is NULL, print [0] (nil)
- */
+ * Return: counts
+*/
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	const list_t *uniq = h;
 
-while (uniq != NULL)
+	while (uniq != NULL)
 	{
-if (uniq->str == NULL)
+		if (uniq->str == NULL)
+			printf("[0] (nil)\n");
 
-	printf("[0] (nil)\n");
-
-	else
+		else
 			printf("[%u] %s\n", uniq->len, uniq->str);
 
 		uniq = uniq->next;
 		count++;
 	}
+
 	return (count);
 }
