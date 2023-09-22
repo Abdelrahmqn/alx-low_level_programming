4150c052af86a5380822d24ef9a6e5da27497a36
Abdelrahman <abdo@example.com>
2023-09-22 09:22:36 -0700

    linked lists in c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 	const list_t *temp = h;
 
 	while (temp != NULL)
@@ -22,8 +22,8 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", temp->len, temp->str);
 
 		temp = temp->next;
-		i++;
+		count++;
 	}
 
-	return (i);
+	return (count);
 }
