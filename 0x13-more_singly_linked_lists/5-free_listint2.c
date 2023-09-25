6a5a217729b41d1771c620a34ba5b51c4c7a8da0
Abdelrahman <abdo@example.com>
2023-09-25 12:08:51 -0700

    linked lists in c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t2 *ptr;
+
+	while (*head)
+	{
+		ptr = *head->next;
+		free(*head);
+		*head = ptr;
+	}
+}
+
