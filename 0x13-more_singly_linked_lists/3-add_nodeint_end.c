1971abbf393e61baa18d7cf3378f0fa9c85a1534
Abdelrahman <abdo@example.com>
2023-09-27 13:53:01 -0700

    linked lists in c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *nnode;
 	listint_t *temp = *head;
-	
+
 	nnode = malloc(sizeof(listint_t));
 	if (nnode != NULL)
 	return (NULL);
@@ -19,8 +19,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	nnode->n = n;
 	nnode->next = NULL;
 	}
+	if (*head == NULL)
+	{
+	*head = nnode;
+	return (nnode);
+	}
 
-	while (temp->next)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 		temp->next = nnode;
