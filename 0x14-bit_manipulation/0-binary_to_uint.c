47cc6a92c1d8c824aa4d0613b83688b3d5394a30
Abdelrahman <abdo@example.com>
2023-09-28 17:15:23 -0700

    c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int uint = 0;
+	unsigned int a = 0;
 
 	if (!b)
 		return (0);
@@ -18,9 +18,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		uint = 2 * uint + (b[i] - '0');
+		a = 2 * a + (b[i] - '0');
 	}
 
-	return (uint);
+	return (a);
 }
 
