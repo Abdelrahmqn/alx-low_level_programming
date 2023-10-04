795a2fa31f516e125169603c67f1f8411882bb3f
Abdelrahman <abdo@example.com>
2023-10-04 15:24:47 -0700

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,8 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
-	fd = fopen(filename, "r");
-	if (fd == -1)
+	fd = fopen(filename, O_RDONLY)
+	if (fd == NULL)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
