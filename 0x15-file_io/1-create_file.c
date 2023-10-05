85b67b1a1759dc9ba2eb1a21b48874730e69d21d
Abdelrahman <abdo@example.com>
2023-10-05 09:34:27 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,11 @@ int create_file(const char *filename, char *text_content)
 	{
 	return (-1);
 	}
-	// stop
 			if (text_content != NULL)
 			{
 				for (len = 0; text_content[len];)
 					len++;
 			}
-//done with length
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
 
 	if (fd == -1)
@@ -29,10 +27,10 @@ int create_file(const char *filename, char *text_content)
 	printf("error");
 	return (-1);
 	}
-	//done with file
 
 	w = write(fd, text_content, len);
-	if (fd == -1) || w == -1
+
+	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
 	return (1);
