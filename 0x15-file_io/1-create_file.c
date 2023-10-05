6444091ba84cbd1e799b7c0786879948473f5537
Abdelrahman <abdo@example.com>
2023-10-05 09:44:42 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 				for (len = 0; text_content[len];)
 					len++;
 			}
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (w == -1 || fd == -1)
