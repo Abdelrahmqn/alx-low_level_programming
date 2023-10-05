7200cde4f2d54a22dda54909d999f7fb3376cf1a
Abdelrahman <abdo@example.com>
2023-10-05 11:39:13 -0700

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,16 +17,18 @@ return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-		if (text_content != NULL)
-		{
-			for (len = 0; text_content[len];)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
 			len++;
-		}
-	w = write(fd, text_content, len);
+	}
+
+	w = write(o, text_content, len);
 
 	if (fd == -1 || w == -1)
-	return (-1);
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
