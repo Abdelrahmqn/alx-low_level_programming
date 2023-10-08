d08311a46dd711ba0d8b2c3985d27ca582b2dcf4
Abdelrahman <abdo@example.com>
2023-10-08 10:23:46 -0700

    c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 char *str_concat(char *s1, char *s2);
 #endif
