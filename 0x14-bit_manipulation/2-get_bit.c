b7d8478a64be6e2c28efd9af3ede67d532a1c5cb
Abdelrahman <abdo@example.com>
2023-10-09 09:52:34 -0700

    c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
+	int btresult;
 
-	if (index > 63)
+	if (index)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	btresult = (n >> index) & 1;
 
-	return (bit_val);
+	return (btresult);
 }
