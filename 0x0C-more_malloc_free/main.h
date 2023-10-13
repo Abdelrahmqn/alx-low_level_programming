6178c2ddf309173b18144a5b35456ddb1be5fa17
Abdelrahamqn <me@example.com>
2023-10-13 07:07:15 -0700

    c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *_strdup(char *str);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
