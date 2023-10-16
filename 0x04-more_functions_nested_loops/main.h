7214bfc4d74ca7e0b2a93a2d644e52bfee0eee94
Abdelrahamqn <me@example.com>
2023-10-16 09:24:28 -0700

    devobs

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 int _putchar(char c);
-
+int main(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
 #endif
