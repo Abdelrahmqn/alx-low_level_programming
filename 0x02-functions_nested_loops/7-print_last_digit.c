edebd564f9bead5591e264c03631ab5bb81995eb
Abdelrahamqn <me@example.com>
2023-10-16 07:01:42 -0700

    devobs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,11 @@ int print_last_digit(int num)
 {
 int last_digit;
 
-	last_digit = num % 10;
-
-	_putchar(last_digit);
+	if (num < 0)
+		num = -num;
+last_digit = num % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
