2a6c55817c2e6f8016a1273fd36a32839f972bed
Abdelrahamqn <me@example.com>
2023-10-25 14:21:26 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number.
+ * _sqrt_helper - returns the natural square root of a number.
  * @i: our digits.
  * @n: natural numbers.
  * Return: return -1 when no natural square root.
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_helper(int n, int i)
 {
-	int i;
-
 	if (i * i == n)
 	{
 		return (i);
@@ -19,16 +17,15 @@ int _sqrt_recursion(int n, int i)
 	}
 	else
 	{
-		return (_sqrt_recursion(n, i + 1));
+		return (_sqrt_helper(n, i + 1));
 	}
 }
 /**
- * _sqrt_helper - returns helper
- * @i: our digits.
+ * _sqrt_recursion - returns helper
  * @n: natural numbers.
  * Return: the value of helper function
  */
-	int _sqrt_helper(int n, int i)
+	int _sqrt_recursion(int n)
 	{
-	return (sqrt_recursion(n, 0));
+	return (_sqrt_helper(n, 0));
 	}
