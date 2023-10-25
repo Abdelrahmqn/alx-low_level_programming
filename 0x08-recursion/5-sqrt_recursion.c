c30794dbd9c3d6a26bfe18d8e71a678521876519
Abdelrahamqn <me@example.com>
2023-10-25 12:48:09 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,34 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number.
+ * _sqrt_helper - returns the natural square root of a number.
  * @i: our digits.
  * @n: natural numbers.
  * Return: return -1 when no natural square root.
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_helper(int n, int i)
 {
+	int i;
+
 	if (i * i == n)
-	{
-		return (-1);
-	}
-	else if (i * i > n)
 	{
 		return (i);
 	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
 	else
 	{
-		return (_sqrt_recursion(n, i + 1));
+		return (_sqrt_helper(n, i + 1));
 	}
 }
+/**
+ * _sqrt_recursion - returns helper
+ * @i: our digits.
+ * @n: natural numbers.
+ * Return: the value of helper function
+ */
+	int _sqrt_recursion(int n, int i)
+	{
+	return (sqrt_helper(n, 0));
+	}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 int main(void);
-int sqrt_helper(int n, int i);
+int _sqrt_helper(int n, int i);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
