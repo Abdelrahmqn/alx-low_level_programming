7a9c8898b8c36a33a49668fdf6f25b086a333e4a
Abdelrahamqn <me@example.com>
2023-10-25 13:42:13 -0700

    c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome - function that string is a palindrome
+ * @s: string fo characters
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s != '\0')
+	{
+	return (1);
+	}
+
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _sqrt_helper - returns the natural square root of a number.
+ * _sqrt_recursion - returns the natural square root of a number.
  * @i: our digits.
  * @n: natural numbers.
  * Return: return -1 when no natural square root.
  */
-int _sqrt_helper(int n, int i)
+int _sqrt_recursion(int n, int i)
 {
 	int i;
 
@@ -19,16 +19,16 @@ int _sqrt_helper(int n, int i)
 	}
 	else
 	{
-		return (_sqrt_helper(n, i + 1));
+		return (_sqrt_recursion(n, i + 1));
 	}
 }
 /**
- * _sqrt_recursion - returns helper
+ * _sqrt_helper - returns helper
  * @i: our digits.
  * @n: natural numbers.
  * Return: the value of helper function
  */
-	int _sqrt_recursion(int n, int i)
+	int _sqrt_helper(int n, int i)
 	{
-	return (sqrt_helper(n, 0));
+	return (sqrt_recursion(n, 0));
 	}
