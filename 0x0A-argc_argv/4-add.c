6d71ae7f808599960e80d72cd885bb1b54e825c0
Abdelrahamqn <me@example.com>
2023-10-27 02:44:59 -0700

    c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,8 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 31; i >= 127; i++)
-		printf("Error\n")
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+	printf("Error\n");
 	return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 + num2;
+	printf("%d\n", result);
+	return (0);
 }
