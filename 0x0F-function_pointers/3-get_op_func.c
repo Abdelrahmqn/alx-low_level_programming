769d0c51b9df9816f3749f1e602c98404ae4d9c8
Abdelrahamqn <abdelrahman@example.com>
2023-11-11 23:04:47 -0800

    c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,13 +19,10 @@ op_t ops[] = {
 	if (s == NULL)
 	return (NULL);
 
-	while (i < 5)
+	for (i = 0; i < 5; i++)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-		{
 			return (ops[i].f);
-		}
-	i++;
-}
+	}
 	return (NULL);
 }
