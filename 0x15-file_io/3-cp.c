b0e529dfbb8a040898b0bd835511c67a098b19f1
Abdelrahmqn <boda8099@gmail.com>
2023-11-27 12:37:37 -0800

    c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,35 +1,38 @@
 #include "main.h"
 /**
- * cp_from_another - function that copies contxt from file to another.
+ * main - function that copies contxt from file to another.
  *
- * @file_from: the file that we will copy the content from.
- *
- * @text_file: the text that will copied.
+ * @argc: arguments.
+ * @argv: argumntsv.
  *
  * Return: 1 on success, -1 on failure.
  */
-int cp_from_another(const char *file_content, char *text_file)
+int main(int argc, char *argv[])
 {
+	const char *file_from;
+	const char *file_to;
 	int f_from, f_to;
-	char buffer[BUF_SIZE];
+	char buffer[1024];
 	ssize_t bytes_write, bytes_read;
 
-	if (file_content == NULL || text_file == NULL)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+			exit(97);
 	}
-	f_from = open(file_content, O_WRONLY);
+	file_from = argv[1];
+	file_to = argv[2];
+	f_from = open(file_from, O_RDONLY);
 	if (f_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", file_from);
 		exit(98);
 	}
-	f_to = open(text_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	f_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (f_to == -1)
 	{
 		close(f_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", file_to);
 		exit(99);
 	}
 	while ((bytes_read = read(f_from, buffer, BUF_SIZE)) > 0)
@@ -39,14 +42,14 @@ int cp_from_another(const char *file_content, char *text_file)
 		{
 			close(f_from);
 			close(f_to);
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", file_to);
 		exit(99);
 		}
 		if (bytes_read == -1)
 		{
 		close(f_from);
 		close(f_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", file_to);
 		exit(98);
 		}
 		if (close(f_from) == -1 || close(f_to) == -1)
