17fcc2442ff13dd285fd7f8818f871a98a9aba6e
abdlerahman mohamed <boda8099@gmail.com>
2024-02-08 23:44:01 +0200

    error handled

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 #include <string.h>
 /**
- * hash_table_set - is function to set a new element to the hash table.
- * @key: is key , the key of the value.
- * @value: is the value of the element that must be duplicated.
- * @ht: is the hash table.
- *
- * Return: on success return 1, otherwise return 0.
- */
+	* hash_table_set - is function to set a new element to the hash table.
+	* @key: is key , the key of the value.
+	* @value: is the value of the element that must be duplicated.
+	* @ht: is the hash table.
+	*
+	* Return: on success return 1, otherwise return 0.
+	*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_element;
@@ -17,19 +17,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 	return (0);
 	}
-
 	new_element = malloc(sizeof(hash_node_t));
-
 	if (new_element == NULL)
 	{
 	free(new_element);
 	return (0);
 	}
-
 	new_element->key = (char *) key;
+	if (new_element->key == NULL)
+	{
+	free(new_element);
+	return (0);
+	}
 	new_element->key = strdup(value);
 	if (new_element->value == NULL)
 	{
+	free(new_element->value);
 	free(new_element);
 	return (0);
 	}
