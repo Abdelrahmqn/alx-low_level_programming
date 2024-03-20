0c1fcf12dab15f2d9b818b044843d9af27457580
abdlerahman mohamed <boda8099@gmail.com>
2024-03-21 00:40:51 +0200

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    0 represents a water zone
+    1 represents a land zone
+    """
+
+    edges = 0
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    the_grid = perimeter * 4 - edges * 2
+    return the_grid
