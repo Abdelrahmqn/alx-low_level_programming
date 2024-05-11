126964ac80ed5e2f9a396568fd64feaf66346c8d
Abdelrahamqn <boda8099@gmail.com>
2024-05-11 03:10:23 +0300

    test

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,9 @@
 #define _SEARCH_ALGOS_H_
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
